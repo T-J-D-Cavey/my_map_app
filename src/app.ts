93e5ee047eb570d79436435e66309ebb77473576
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-08-14 19:17:51 +0100

    set up event ts event listeners

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHander(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    console.log(enteredAddress);
+}
+
+form.addEventListener('submit', searchAddressHander);
+

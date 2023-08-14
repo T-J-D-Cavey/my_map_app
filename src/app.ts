e07cafbeacc4ee9342a708f21a8842c17b832a98
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-08-14 20:49:47 +0100

    set up google geo locator logic and axios.get logic

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import {API_KEY} from './api';
+import axios from 'axios';
+import {API_KEY as GOOGLE_API_KEY} from './api';
 
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
@@ -6,7 +7,12 @@ const addressInput = document.getElementById('address')! as HTMLInputElement;
 function searchAddressHander(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
-    console.log(enteredAddress, API_KEY);
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+        console.log(response);
+    }).catch(err => {
+        console.log(err);
+    })
 }
 
 form.addEventListener('submit', searchAddressHander);

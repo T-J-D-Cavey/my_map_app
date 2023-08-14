c0dda3674464aff4b6013c6355117c2bdebde41a
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-08-14 21:10:53 +0100

    set up generic type for geocoding response

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import {API_KEY as GOOGLE_API_KEY} from './api';
 
+type GoogleGeocodingResponse = {
+    results: {geometry: {location: {lat: number, lng: number}}}[]
+}
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 function searchAddressHander(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
     .then(response => {
-        console.log(response);
+        const coordinates = response.data.results[0].geometry.location;
     }).catch(err => {
         console.log(err);
     })

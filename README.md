2e2e282881391876f631cdf97aec35adf641f4cf
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-08-14 22:37:52 +0100

    final commit before pushing to remote repository

---
+++ README.md
@@ -0,0 +1,17 @@
+# My Map App
+
+A map web app using the Google geolocator and maps API with typescript.
+
+## What is this project made with?
+
+- HTML
+- CSS
+- Typescript / Javascript ES6
+- Webpack / Webpack CLI / Webpack devserver
+- Node / NPM
+- Google maps API
+- Axios
+
+## Why was this project made?
+
+This is another practice project originally designed by Maximilian Schwarzmuller, to practice Typescript with third party libraries. It was part of a Udemy course on Typescript.
--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,6 @@ type GoogleGeocodingResponse = {
     status: 'OK' | 'ZERO_RESULTS' | 'INVALID_REQUEST' | 'UNKNOWN_ERROR' | 'REQUEST_DENIED';
 }
 
-declare var google: any;
-
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
@@ -20,9 +18,9 @@ function searchAddressHander(event: Event) {
             throw new Error(`${response.data.status}`);
         }
         const coordinates = response.data.results[0].geometry.location;
-        const map = new google.maps.Map(document.getElementById("map"), {
+        const map = new google.maps.Map(document.getElementById("map")!, {
             center: coordinates,
-            zoom: 8,
+            zoom: 12,
           });
         new google.maps.Marker({position: coordinates, map: map})
     }).catch(err => {
